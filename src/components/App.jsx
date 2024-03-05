7de5773bfef43ec1f032f41242915618f4e029c9
Babenko Liudmyla <savelieva0509@gmail.com>
2024-03-05 18:03:42 +0200

    add task list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import TaskForm from 'components/TaskForm/task-form'
 import Layout from 'components/Layout/layout'
+import TaskList from './TaskList/task-list'
 
 const App = () => {
   return (
     <Layout>
       <TaskForm />
+      <TaskList />
     </Layout>
   )
 }
