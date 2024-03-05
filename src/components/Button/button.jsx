2d5aa6824ac397bc33f78a9a575f559e5f322f30
Babenko Liudmyla <savelieva0509@gmail.com>
2024-03-05 18:03:53 +0200

    fix

--- src/components/Button/button.jsx
+++ src/components/Button/button.jsx
@@ -1,7 +1,11 @@
 import css from './button.module.css'
 
-const Button = ({ type }) => {
-  return <button className={css.btn} type={type}></button>
+const Button = ({ type, children }) => {
+  return (
+    <button className={css.btn} type={type}>
+      {children}
+    </button>
+  )
 }
 
 export default Button
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from 'components/app'
+import React from 'react' // eslint-disable-line no-unused-vars
+import ReactDOM from 'react-dom/client' // eslint-disable-line no-unused-vars
+import App from 'components/app' // eslint-disable-line no-unused-vars
 import './index.css'
 
 ReactDOM.createRoot(document.querySelector('#root')).render(
