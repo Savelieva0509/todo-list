28c747c96af0673b5b929d584e14977d7d4e2cf1
Babenko Liudmyla <savelieva0509@gmail.com>
2024-03-06 18:31:58 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import { store } from './redux/store' // eslint-disable-line no-unused-vars
 import App from 'components/app' // eslint-disable-line no-unused-vars
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(  
-<Provider store={store}>  
-<App />  
-</Provider>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
 )
