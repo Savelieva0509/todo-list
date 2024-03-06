6d1c1fa94f5bfaf152f8f6e4336d1ab0e3e64d4a
Babenko Liudmyla <savelieva0509@gmail.com>
2024-03-06 12:44:46 +0200

    create store and actions

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react' // eslint-disable-line no-unused-vars
 import ReactDOM from 'react-dom/client' // eslint-disable-line no-unused-vars
-import { Provider } from "react-redux" // eslint-disable-line no-unused-vars
-import { store } from "./redux/store" // eslint-disable-line no-unused-vars
+import { Provider } from 'react-redux' // eslint-disable-line no-unused-vars
+import { store } from './redux/store' // eslint-disable-line no-unused-vars
 import App from 'components/app' // eslint-disable-line no-unused-vars
 import './index.css'
 
-ReactDOM.createRoot(document.querySelector('#root')).render(
-  <React.StrictMode>
-    <Provider store={store}>  
-      <App />
-      </Provider>
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")).render(  
+<Provider store={store}>  
+<App />  
+</Provider>
 )
