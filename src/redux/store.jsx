06e7d60dd621099b6deb32ff85c2f74364b088bf
Babenko Liudmyla <savelieva0509@gmail.com>
2024-03-06 20:37:56 +0200

    fix deploy

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import { tasksReducer } from './tasks-slice'
 
 export const store = configureStore({
-  reducer: {
-    tasks: tasksReducer,
+  'reducer': {
+    'tasks': tasksReducer,
   },
 })
