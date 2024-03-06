ca1247479f183f2e13041729f4cfd6ce9c464c2b
Babenko Liudmyla <savelieva0509@gmail.com>
2024-03-06 17:49:55 +0200

    fix

--- src/components/TaskList/task-list.jsx
+++ src/components/TaskList/task-list.jsx
@@ -1,7 +1,10 @@
+//import { useSelector } from 'react-redux'
 import Task from 'components/Task/task'
+import { getTasks } from 'redux/selectors'
 import css from './task-list.module.css'
 
 const TaskList = () => {
+  //const tasks = useSelector(getTasks)
   return (
     <ul className={css.list}>
       {[].map((task) => (
