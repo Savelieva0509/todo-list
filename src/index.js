070c67e045939266b664c0b8935e5a93e565cddd
Babenko Liudmyla <savelieva0509@gmail.com>
2024-03-07 23:14:28 +0200

    add linter and fix lint errors

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react' 
-import ReactDOM from 'react-dom/client' 
-import { Provider } from 'react-redux' 
-import { store } from './redux/store' 
-import App from './app' 
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
+import App from './app'
 import './index.css'
 
 ReactDOM.createRoot(document.querySelector('.root-container')).render(
-   <React.StrictMode>
+  <React.StrictMode>
     <Provider store={store}>
-    <App />
-  </Provider>
-   </React.StrictMode>
+      <App />
+    </Provider>
+  </React.StrictMode>,
 )
