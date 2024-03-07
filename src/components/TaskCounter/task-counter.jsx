11ee9b2ea87ee84f0875bbd88fd0b0f5ae29d907
Babenko Liudmyla <savelieva0509@gmail.com>
2024-03-07 22:43:40 +0200

    add counter

--- src/components/TaskCounter/task-counter.jsx
+++ src/components/TaskCounter/task-counter.jsx
@@ -1,10 +1,28 @@
+import { useSelector } from 'react-redux'
+import { getTasks } from '.././../redux/selectors'
 import css from './task-counter.module.css'
 
 const TaskCounter = () => {
+  const tasks = useSelector(getTasks)
+  const count = tasks.reduce(
+    (acc, task) => {
+      if (task.completed) {
+        acc.completed += 1
+      } else {
+        acc.active += 1
+      }
+      return acc
+    },
+    { active: 0, completed: 0 }
+  )
   return (
     <div>
-      <p className={css.text}>Active: 0</p>
-      <p className={css.text}>Completed: 0</p>
+      <p className={css.text}>
+        Active: <span className={css.number}>{count.active}</span>
+      </p>
+      <p className={css.text}>
+        Comleted: <span className={css.number}>{count.completed}</span>
+      </p>
     </div>
   )
 }
--- src/redux/tasks-slice.jsx
+++ src/redux/tasks-slice.jsx
@@ -1,11 +1,11 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit'
 
 const tasksInitialState = [
-  { id: 0, text: 'Learn HTML and CSS', completed: true },
-  { id: 1, text: 'Get good at JavaScript', completed: true },
-  { id: 2, text: 'Master React', completed: false },
-  { id: 3, text: 'Discover Redux', completed: false },
-  { id: 4, text: 'Build amazing apps', completed: false },
+  { id: 0, text: 'Do the dishes', completed: true },
+  { id: 1, text: 'Take out the trash', completed: true },
+  { id: 2, text: 'Clean the house', completed: false },
+  { id: 3, text: 'Organize the closet', completed: false },
+  { id: 4, text: 'Cook a delicious meal', completed: false },
 ]
 
 const tasksSlice = createSlice({
